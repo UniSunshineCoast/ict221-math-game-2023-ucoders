8823b19f2550a00a2945fea70b7d2b20c4225841
Ming <mwang@usc.edu.au>
2023-03-19 16:27:07 +1000

    Lab 2 implemented

--- src/au/edu/usc/mathgame/Main.java
+++ src/au/edu/usc/mathgame/Main.java
@@ -14,19 +14,20 @@ public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
+        int score = 0;
 
         // now ask some random addition questions.
         for (int i = 0; i < 10; i++) {
-            int a = rand.nextInt(20);
-            int b = rand.nextInt(20);
-            int correct = a + b;
-            System.out.printf("What is %2d + %2d? ", a, b);
+            Question q = new Question(rand);
+            q.showQuestion();
             int response = input.nextInt();
-            if (response == correct) {
-                System.out.printf("  Yes!\n");
-            } else {
-                System.out.printf("  No, the answer is %d.\n", correct);
+
+            boolean good = q.checkAnswer(response);
+            if (good) {
+                score++;
             }
         }
+
+        System.out.printf("You have answered %2d out of 10 questions correctly.\n", score);
     }
 }
